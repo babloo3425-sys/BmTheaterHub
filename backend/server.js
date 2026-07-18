f156189632441930c4581a79e871c2c8fa2d188c
Your Name <you@example.com>
2026-07-18 19:11:47 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,6 @@ dotenv.config({
 });
 
 // =====================================================
-// Debug (Temporary)
-// NOTE:
-// Publish se pehle ye console.log remove kar denge.
-// =====================================================
-console.log(process.env.MONGO_URI);
-
-console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API Key:", process.env.CLOUDINARY_API_KEY);
-console.log(
-    "API Secret:",
-    process.env.CLOUDINARY_API_SECRET ? "Loaded" : "Missing"
-);
-
-// =====================================================
 // Import Database
 // =====================================================
 const connectDB = require("./config/db");
